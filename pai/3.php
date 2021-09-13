bffc3de41bd5d5dc4aeab54bec26567f8a600872
kwonbx <89992779+kwonbx@users.noreply.github.com>
2021-09-13 08:18:04 +0200

    Create 3.php

---
+++ pai/3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="urf-8">
+    <title></title>
+  </head>
+  <body>
+      Zawartość strony<hr>
+    <?php
+      require '/3_1.php';
+      include '/3_1.php';
+      include_once '/3_1.php';
+      require_once '/3_1.php';
+    ?>
+      <hr>Koniec pliku
+  </body>
+</html>
