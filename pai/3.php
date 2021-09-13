6ff913f15c9235b7cd772916fe122f0f179e82e9
kwonbx <89992779+kwonbx@users.noreply.github.com>
2021-09-13 19:24:11 +0200

    Update 3.php

--- pai/3.php
+++ pai/3.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="pl" dir="ltr">
   <head>
-    <meta charset="urf-8">
+    <meta charset="utf-8">
     <title></title>
   </head>
   <body>
-      Zawartość strony<hr>
+      Zawartość strony:<hr>
     <?php
       require '/3_1.php';
       include '/3_1.php';
