88ea16d45770f37822d8cb72f8ead298616ea944
kwonbx <89992779+kwonbx@users.noreply.github.com>
2021-09-13 19:22:34 +0200

    Update 1.php

--- pai/1.php
+++ pai/1.php
@@ -1,113 +1,54 @@
 <?php
-echo "Text<br><t>";
-$name="Krystyna";
-echo "Imię: $name<br>";
+  echo "text<br>";
+  echo 'text<br>';
+  $name="Krystyna";
+  echo 'Imię: $name<br>'; //Imię: $name
+  echo "Imię: $name<br>"; //Imię: $name
 
-//typy danych!!
+// typy danych
+// całkowite
+  $calkowita=10;
+  $bin=0b1010; //10
+  $oct=011; //9
+  $hex=0x11; //17
+  echo $hex;
 
+// zmiennoprzecinkowy
+  $x=10.5;
+  echo gettype($x); //double
 
-//całkowite
-$całkowita=10;
-$bin=0b1010; //10
-$oct=011; //9
-$hex=0x11; //17
+// logiczna
+  $prawda=true;
+  $fałsz=false; //nie zapisujemy nazw zmiennych polskimi znakami
 
+  echo $prawda; //1
+  echo $fałsz; //nic nie wyświetli
 
-//zmiennoprzecinkowe
-$x=10.5;
-echo gettype($x);
-
-
-//logiczna
-$prawda=true;
-$fałsz=false;
-
-
-//składnia heredoc
-//służy do wyświetlenia danych
-echo <<< E
-<hr> <br>Imię: $name<br><br>
-ZSŁ
-<hr>
+// składnia heredoc
+  echo <<< E
+    <hr>Imię: $name<br>
+    ZSŁ
+    <hr>
 E;
 
+  $text = <<< E
+    <hr>Imię: $name<br>
+    ZSŁ
+    <hr>
+E;
 
-//2 wersja heredoca (nie działa mi)
-$text = <<< P
-<hr> <br>Imię: $name<br><br>
-ZSŁ
-<hr>
-P;
+echo $text;
 
-
-//nowdoc
+// nowdoc
 echo <<< 'E'
-<hr>Imię: $name<br>
-ZSŁ
-<br><br><hr>
+  <hr>Imię: $name<br>
+  ZSŁ
+  <hr>
 E;
 
+// konkatenacja
+  echo "text1"."text2";
 
-//konkatenacja
-echo "text1"."text2";
-//dzięki backslashowi traktuje zmienną jako stringa
-echo "<br><br>Imię: \$name"; 
+echo "Imię: \$name"; //Imię: $name
 
-echo "test<br><br>";
-echo 2**10, '<br>';
-
-
-//porównywanie (1 lub -1 w outpucie)
-$x=1000;
-$y=100;
-echo $x<=>$y;
-
-
-//równe lub identyczne
-$x=1;   //jako integer
-$y=1.0; //jako double
-if($x==$y)
-{
-    echo "<br>Equal";
-
-}
-else
-{
-    echo "<br>Not Equal";
-}
-
-
-//równe lub identyczne + typ danych
-$x=1;   //jako integer działa
-$y=1.0; //jako double działa
-if($x===$y)
-{
-    echo "<br>Equal";
-
-}
-else
-{
-    echo "<br>Not Equal"; //wynik to "Not Equal" bo różne typy danych (int and double)
-}
-
-/*
-preinkrementacja ++$x
-postinkrementacja $x++
-predekrementacja --$x
-postdekrementacja $x--
-*/
-
-$x=1;
-echo $x; //1
-$x=$x++;
-echo $x; //1
-$x=++$x;
-echo $x; //2
-$y=++$x;
-echo $x; //3
-echo $y; //3
-$y=$x++;
-echo $x; //4
-echo "$y<br>"; //3
-
-?>
+  ?>
