90b6af2fd18c35be2acff7f580db5eb86035f845
kwonbx <89992779+kwonbx@users.noreply.github.com>
2021-09-27 18:18:13 +0200

    Create 5_1_form.php

---
+++ pai/5_1_form.php
@@ -0,0 +1,15 @@
+  
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="5_1_data.php" method="get">
+      <input type="number" name="age" placeholder="Podaj wiek"><br><br>
+      <input type="text" name="name" placeholder="Podaj imię"><br><br>
+      <input type="submit" value="submit">
+    </form>
+  </body>
+</html>
