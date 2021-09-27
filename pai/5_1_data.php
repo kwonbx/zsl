e75758a693e878fca10a99b87d53cd0cefb20689
kwonbx <89992779+kwonbx@users.noreply.github.com>
2021-09-27 18:17:40 +0200

    Create 5_1_data.php

---
+++ pai/5_1_data.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      if (!empty($_GET['age']) && !empty($_GET['name'])) {
+        $name = ucfirst(strtolower($_GET['name']));
+        echo <<< L
+        <h3>Dane użytkownika pobrane z formularza</h3>
+        Imię: $name<br>
+        Wiek: $_GET[age]
+L;
+      }
+     ?>
+  </body>
+</html>
